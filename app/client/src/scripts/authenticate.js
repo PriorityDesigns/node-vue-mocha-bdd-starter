326a1fe3503ac5fbe5a3d04672f8c88702317793
amorgan <amorgan@prioritydesigns.com>
2022-06-17 14:21:31 -0400

    Removing authenticate example

--- app/client/src/scripts/authenticate.js
+++ app/client/src/scripts/authenticate.js
@@ -2,27 +2,7 @@
 export default {
   hasError: false,
   loading: false,
-  authenticateUser(username, password) {
-    let headers = { 'Content-type': 'application/json' };
-    this.loading = true;
+  authenticateUser() {
 
-    try {
-      const response = {
-        data: {
-          isAuthenticated: password == "1234"
-        }
-      };
-      this.loading = false;
-      return response.data.isAuthenticated;
-    }
-    catch (error) {
-      console.log("API Error: " + error);
-      console.log("API Error: " + error.response.data);
-      this.hasError = true;
-      this.loading = false;
-      return error.response.data;
-    }
-
-    
   }
 }
